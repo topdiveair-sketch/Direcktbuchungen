ef784285c290fa2e41c27bddde203b4bc5bf28fe
topdiveair-sketch <topdiveair@gmail.com>
2026-09-08 20:37:05 +0200

    Initialize local database on Windows app startup

--- windows/WachauEtappe.Zentrale/App.xaml.cs
+++ windows/WachauEtappe.Zentrale/App.xaml.cs
@@ -1,7 +1,17 @@
 using System.Windows;
+using WachauEtappe.Zentrale.Data;
 
 namespace WachauEtappe.Zentrale;
 
 public partial class App : Application
 {
+    public static DatabaseService Database { get; private set; } = null!;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Database = new DatabaseService();
+        Database.Initialize();
+        new SeedImporter(Database).ImportAll();
+        base.OnStartup(e);
+    }
 }
