9b1d743b1c352a2871ba1654d4641f30549c2b02
topdiveair-sketch <topdiveair@gmail.com>
2026-09-08 22:00:14 +0200

    Fix font creation in WachauEtappe icon generator

--- windows/WachauEtappe.Zentrale/tools/New-WachauEtappeIcon.ps1
+++ windows/WachauEtappe.Zentrale/tools/New-WachauEtappeIcon.ps1
@@ -7,7 +7,7 @@ $dir = Split-Path -Parent $OutputPath
 New-Item -ItemType Directory -Force -Path $dir | Out-Null
 
 function New-IconPng([int]$Size) {
-  $bmp = New-Object System.Drawing.Bitmap $Size, $Size
+  $bmp = [System.Drawing.Bitmap]::new($Size,$Size)
   $g = [System.Drawing.Graphics]::FromImage($bmp)
   $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
   $g.Clear([System.Drawing.Color]::Transparent)
@@ -15,31 +15,31 @@ function New-IconPng([int]$Size) {
   $green = [System.Drawing.Color]::FromArgb(23,61,50)
   $gold  = [System.Drawing.Color]::FromArgb(220,176,73)
   $white = [System.Drawing.Color]::White
-  $bgBrush = New-Object System.Drawing.SolidBrush $green
+  $bgBrush = [System.Drawing.SolidBrush]::new($green)
   $g.FillEllipse($bgBrush, 2, 2, $Size-4, $Size-4)
 
-  $pen = New-Object System.Drawing.Pen $gold, ([Math]::Max(2,$Size/18))
+  $pen = [System.Drawing.Pen]::new($gold,[single]([Math]::Max(2,$Size/18)))
   $pen.StartCap = [System.Drawing.Drawing2D.LineCap]::Round
   $pen.EndCap   = [System.Drawing.Drawing2D.LineCap]::Round
   [System.Drawing.Point[]]$pts = @(
-    (New-Object System.Drawing.Point ([int]($Size*0.18)),([int]($Size*0.70))),
-    (New-Object System.Drawing.Point ([int]($Size*0.36)),([int]($Size*0.42))),
-    (New-Object System.Drawing.Point ([int]($Size*0.50)),([int]($Size*0.58))),
-    (New-Object System.Drawing.Point ([int]($Size*0.70)),([int]($Size*0.28))),
-    (New-Object System.Drawing.Point ([int]($Size*0.83)),([int]($Size*0.43)))
+    [System.Drawing.Point]::new([int]($Size*0.18),[int]($Size*0.70)),
+    [System.Drawing.Point]::new([int]($Size*0.36),[int]($Size*0.42)),
+    [System.Drawing.Point]::new([int]($Size*0.50),[int]($Size*0.58)),
+    [System.Drawing.Point]::new([int]($Size*0.70),[int]($Size*0.28)),
+    [System.Drawing.Point]::new([int]($Size*0.83),[int]($Size*0.43))
   )
   $g.DrawLines($pen,$pts)
 
   $fontSize = [Math]::Max(7,[int]($Size*0.20))
-  $font = New-Object System.Drawing.Font 'Segoe UI Semibold',$fontSize,[System.Drawing.FontStyle]::Bold,[System.Drawing.GraphicsUnit]::Pixel
-  $brush = New-Object System.Drawing.SolidBrush $white
-  $sf = New-Object System.Drawing.StringFormat
+  $font = [System.Drawing.Font]::new('Segoe UI Semibold',[single]$fontSize,[System.Drawing.FontStyle]::Bold,[System.Drawing.GraphicsUnit]::Pixel)
+  $brush = [System.Drawing.SolidBrush]::new($white)
+  $sf = [System.Drawing.StringFormat]::new()
   $sf.Alignment = [System.Drawing.StringAlignment]::Center
   $sf.LineAlignment = [System.Drawing.StringAlignment]::Center
-  $rect = New-Object System.Drawing.RectangleF 0,($Size*0.60),$Size,($Size*0.28)
+  $rect = [System.Drawing.RectangleF]::new(0,[single]($Size*0.60),[single]$Size,[single]($Size*0.28))
   $g.DrawString('WE',$font,$brush,$rect,$sf)
 
-  $ms = New-Object System.IO.MemoryStream
+  $ms = [System.IO.MemoryStream]::new()
   $bmp.Save($ms,[System.Drawing.Imaging.ImageFormat]::Png)
   $bytes = $ms.ToArray()
   $g.Dispose(); $bmp.Dispose(); $font.Dispose(); $brush.Dispose(); $bgBrush.Dispose(); $pen.Dispose(); $sf.Dispose(); $ms.Dispose()
@@ -51,7 +51,7 @@ $images = @()
 foreach($s in $sizes){ $images += ,(New-IconPng $s) }
 
 $fs = [System.IO.File]::Open($OutputPath,[System.IO.FileMode]::Create)
-$bw = New-Object System.IO.BinaryWriter $fs
+$bw = [System.IO.BinaryWriter]::new($fs)
 $bw.Write([UInt16]0); $bw.Write([UInt16]1); $bw.Write([UInt16]$sizes.Count)
 $offset = 6 + (16 * $sizes.Count)
 for($i=0;$i -lt $sizes.Count;$i++){
